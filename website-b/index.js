7a91e434bc07c211ebae4293e53db24118e989d3
kikipuspita <151821419+kikipuspita@users.noreply.github.com>
2023-12-01 23:22:37 +0700

    first commit

---
+++ website-b/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var jsonParser = bodyParser.json();
+
+var urlencodeParser = bodyParser.urlencoded({ extended: false });
+
+const port = 3001;
+
+app.use(cors());
+app.use(jsonParser);
+app.use(urlencodeParser);
+
+app.get("/", async (req, res) => {
+  const response = await fetch("http://localhost:3000/");
+  const body = await response.text();
+
+  console.log(body);
+  res.json("website-b");
+});
+
+app.get("/trigger-webhook-event", async (req, res) => {
+  try {
+    const data = {
+      secret: "secret123",
+    };
+
+    const response = await fetch("http://localhost:3000/github-event", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    res.json("success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
